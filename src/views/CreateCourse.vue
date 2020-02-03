29f5b2887c0e28c183d695c9048b5ead5b78b687
A7103 <2907219148@qq.com>
2020-02-03 18:57:53 +0800

    登录和注册页面

--- src/views/CreateCourse.vue
+++ src/views/CreateCourse.vue
@@ -2,65 +2,16 @@
   <div>
     <!--头部部分 start-->
     <div style="margin-top: 2%;height: 10%">
-      <h4 class="talk_title">课程</h4>
+      <h4 class="talk_title">讨论</h4>
       <div class="divide_1"/>
       <div style="height: 5%">
-        <h5 class="msg_title">新建课程</h5>
+        <h5 class="msg_title">信息</h5>
       </div>
       <hr class="divide_2">
     </div>
     <!--头部部分 end-->
+    <div style="margin-top: 2%;height: 90%">
 
-
-    <div style="margin-top: 3%;height: 90%;">
-      <div style="width: 50%;min-height: 500px;margin-right: 5%;margin-left: 5%; position:absolute;">
-         <!--课程名称 start-->
-        <el-form :model="createCourseForm" label-width="80px" ref="form" size="medium">
-        <el-form-item label="课程名称">
-          <el-input style="width: 250px" v-model="createCourseForm.courseName" placeholder="请输入课程名" clearable></el-input>
-        </el-form-item>
-        <!--课程名称 end-->
-        <!--截止日期 start-->
-        <el-form-item label="截止日期">
-          <div class="block">
-            <el-date-picker
-              style="width: 250px"
-              v-model="deadline"
-              type="daterange"
-              range-separator="至"
-              start-placeholder="开始日期"
-              end-placeholder="结束日期">
-            </el-date-picker>
-          </div>
-        </el-form-item>
-        <!--截止日期 end-->
-
-          <!--课时选择 start-->
-          <el-form-item label="课时数量">
-          <el-input-number v-model="num" controls-position="right" @change="handleChange" :min="1" :max="48" style="width:100px"></el-input-number>
-          </el-form-item>
-          <!--课时选择 end-->
-
-          <!--课程描述 start-->
-          <el-form-item label="课程描述">
-            <el-input
-              style="width: 350px"
-              type="textarea"
-              resize="none"
-              :autosize="{ minRows: 4, maxRows: 8}"
-              placeholder="请输入内容"
-              v-model="courseDescription">
-            </el-input>
-          </el-form-item>
-          <!--课程描述 end-->
-          <!--按钮 start-->
-          <el-form-item size="large">
-            <el-button @click="onSubmit" type="danger" style="width: 180px;margin-right: 5%">提交</el-button>
-            <el-button style="width: 180px">取消</el-button>
-          </el-form-item>
-          <!--按钮 end-->
-      </el-form>
-    </div>
 
     </div>
   </div>
@@ -68,36 +19,16 @@
 
 <script>
   export default {
-    name: 'CreateCourse',
-    data() {
-      return {
-        createCourseForm: {
-          courseName: '',
-        },
-        deadline: '',
-        courseDescription: '',
-        num: 1
-      };
-    },
-
-    methods: {
-      onSubmit() {
-        console.log('submit!');
-      },
-      handleChange(value) {
-        console.log(value);
-      }
-    }
-  };
+    name: 'CreateCourse'
+  }
 </script>
 
 <style scoped>
   .talk_title {
-    font-family: 微软雅黑, serif;
+    font-family: 黑体, serif;
     margin-left: 5%;
     color: #f55d54;
     text-align: left;
-    font-size: 20px;
   }
 
   .divide_1 {
@@ -112,7 +43,6 @@
     margin-top: 1%;
     margin-left: 5%;
     text-align: left;
-    font-size: 15px;
   }
 
   .divide_2 {
