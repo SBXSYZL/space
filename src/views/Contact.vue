09bfe4dae7d31d32aaadf64a48a828256a900f34
A7103 <2907219148@qq.com>
2020-02-03 18:56:33 +0800

    登录和注册页面

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    联系人
+    <!--头部部分 start-->
+    <div style="margin-top: 2%;height: 10%">
+      <h4 class="talk_title">讨论</h4>
+      <div class="divide_1"/>
+      <div style="height: 5%">
+        <h5 class="msg_title">信息</h5>
+      </div>
+      <hr class="divide_2">
+    </div>
+    <!--头部部分 end-->
+    <div style="margin-top: 2%;height: 90%">
+
+
+    </div>
   </div>
 </template>
 
@@ -11,5 +24,29 @@
 </script>
 
 <style scoped>
+  .talk_title {
+    font-family: 黑体, serif;
+    margin-left: 5%;
+    color: #f55d54;
+    text-align: left;
+  }
 
+  .divide_1 {
+    margin-top: 10px;
+    height: 5px;
+    margin-left: 10px;
+    margin-right: 10px;
+    background: #f3f3f3;
+  }
+
+  .msg_title {
+    margin-top: 1%;
+    margin-left: 5%;
+    text-align: left;
+  }
+
+  .divide_2 {
+    margin-top: 1%;
+    color: #f3f3f3;
+  }
 </style>
